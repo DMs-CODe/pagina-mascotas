67fa3c3ba3efd2aa2a7fa186419b6038783edd32
Daniel Marquez <marquez.daniel@uabc.edu.mx>
2020-05-15 22:04:52 -0700

    Se genero el header, se genero cuerpo materialize CSS

---
+++ app.js
@@ -0,0 +1,23 @@
+
+// Eventos para efecto Parallax
+let parallax=document.querySelector('.parallax');
+let title=document.querySelector('.title');
+
+function scrollParallax(){
+    let scrollTop=document.documentElement.scrollTop;
+    parallax.style.transform ='translateY('+ scrollTop * -0.7 +'px)';
+    // title.style.transform ='translateY('+ scrollTop * .05 +'px)';
+
+}
+window.addEventListener('scroll',scrollParallax);
+
+
+// Eventos para Materiallaze CSS
+document.addEventListener('DOMContentLoaded', ()=>{
+  const imgBox = document.querySelectorAll('.materialBoxed')
+  M.Materialbox.init(imgBox, {
+      inDuration: 400,
+      outDuration: 300,
+  });
+
+});
